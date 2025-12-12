852286e6822ddcf629f1cea20d847d7ffd2bdffd
ardnsptra <putrasaga4@gmail.com>
2025-12-12 10:45:13 +0800

    COTS#2

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -319,16 +319,19 @@
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
                 remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                
               </p>
             </div>
-          </div>
-          <div class="lg:w-[50%]">
+              <div class="lg:w-[50%]">
               <div
                 class="bg-black/40 rounded-xl h-64 md:h-80 lg:h-[300px]
                        overflow-hidden shadow-2xl"
               >
+              <img :src="fPantai" :alt="name" class="object-cover w-full h-full" />
               </div>
             </div>
+          </div>
           <FacilityGrid :list="cotscard" />
         </PageSection>
       </div>
